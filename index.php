e70ed6097b00af53dfdfc7df0b45b68681dcfdd5
sivar <sivar@2a077918-f434-4ae4-a93c-d96c61136af1>
2016-07-05 15:37:02 +0000

    integrating slim code with angular app
    git-svn-id: http://svn.enterpi.com/repos/Mintmesh_enterprise/branches/slimintegration@8441 2a077918-f434-4ae4-a93c-d96c61136af1

--- index.php
+++ index.php
@@ -1,61 +1,20 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<!-- This is new comment line for testing SVN -->
-<html ng-app="app">
-    <head>
-        <title>MintMesh</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"> -->
-        <link rel="shortcut icon" href="public/images/favicon.png"/>
-        <link rel="stylesheet" type="text/css" href="public/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="public/css/font-awesome.min.css">
-        <link rel="stylesheet" href="public/css/angular-material.min.css">
-        <link rel="stylesheet" type="text/css" href="public/css/angular-ui-grid.css">
-        <link rel="stylesheet" type="text/css" href="public/css/home.css">
-        <link rel="stylesheet" type="text/css" href="public/css/import-contacts.css">
-        <link rel="stylesheet" type="text/css" href="public/css/fileuploader.css">
-        <script type="text/javascript" src="public/js/vendor/jquery.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="public/js/vendor/angular.js"></script>
-        <script src="public/js/vendor/angular-animate.min.js"></script>
-        <script src="public/js/vendor/angular-aria.min.js"></script>
-        <script src="public/js/vendor/angular-messages.min.js"></script>
-        <script src="public/js/vendor/angular-material.min.js"></script>
-        <script type="text/javascript" src="public/js/vendor/angular-ui-router.js"></script>
-        <script type="text/javascript" src="public/js/vendor/angular-ui-grid.js"></script>
-        <script src="public/js/vendor/ui-bootstrap-tpls.js"></script>
-        <script type="text/javascript" src="public/js/vendor/jquery.easy-autocomplete.js"></script>
-        <script type="text/javascript" src="public/js/vendor/fileuploader.js"></script>
-        <script type="text/javascript">
-            (function () {
-                "use strict";
-                
-                angular.module('app.constants', [])
-                .constant('App', {
-                    base_url: 'http://<?php echo $_SERVER['SERVER_NAME']; ?>/mintmesh_enterprise/development/'
+<?php
+require __DIR__ . '/vendor/autoload.php';
 
-                });
-            }());
-        </script>
-    </head>
-    <body>
-        <div data-ui-view style="height: 100%" ng-cloak></div>
-        
-        <script type="text/javascript" src="public/js/controllers/home.js"></script>
-        <script type="text/javascript" src="public/js/controllers/company-profile.js"></script>
-        <script type="text/javascript" src="public/js/controllers/import-contacts.js"></script>
-        <script type="text/javascript" src="public/js/controllers/emails.js"></script>
-        <script type="text/javascript" src="public/js/controllers/dashboard.js"></script>
-        <script type="text/javascript" src="public/js/controllers/engagement-contacts.js"></script>
-        <script type="text/javascript" src="public/js/controllers/post-job.js"></script>
-        <script type="text/javascript" src="public/js/controllers/job-search.js"></script>
-        <script type="text/javascript" src="public/js/controllers/job-details.js"></script>
-        <script type="text/javascript" src="public/js/controllers/rewards.js"></script>
-        <script type="text/javascript" src="public/js/app.js"></script>
-    </body>
-</html>
+session_start();
+
+// Instantiate the app
+require __DIR__ . '/src/config.php';
+$app = new \Slim\App($settings);
+
+// Set up dependencies
+require __DIR__ . '/src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/src/middleware.php';
+
+// Register routes
+require __DIR__ . '/src/routes.php';
+
+// Run app
+$app->run();
