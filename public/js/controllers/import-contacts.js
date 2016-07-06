8cac8ea6398d7b8bc049a3048896bdf8c7e9e7a9
jayakrishna <jayakrishna@2a077918-f434-4ae4-a93c-d96c61136af1>
2016-07-06 14:17:38 +0000

    changes done on modal window for file uploading
    git-svn-id: http://svn.enterpi.com/repos/Mintmesh_enterprise/branches/dev_with_slim@8463 2a077918-f434-4ae4-a93c-d96c61136af1

--- public/js/controllers/import-contacts.js
+++ public/js/controllers/import-contacts.js
@@ -10,8 +10,7 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
 
   this.selectFile = function(){
       $window.scrollTo(0,0);
-      $state.go('importContactsList');
-      
+      $state.go('importContactsList');     
   }
 
   this.backToCompleteProfile = function() {
@@ -21,7 +20,8 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
 
   this.fileUploadModal = function  (bucketName,color) {
       $uibModal.open({
-        animation: false,
+        animation: true,
+        backdrop: 'static',
         templateUrl: 'templates/dialogs/fileUpload.phtml',
         resolve: {
           bucket: function () {
@@ -40,104 +40,74 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
 
 }])
 
-.controller("fileUpload", ["bucket", function(bucket){
+.controller("fileUpload", ["$uibModalInstance", "$scope","bucket", function($uibModalInstance, $scope, bucket){
     this.bucket = bucket;
-    var color=this.bucket.color;
+    var color = this.bucket.color;
+    var bucketName = this.bucket.name;
+
     this.obj={
       "border":"1px solid "+color,
       "color":color
     }
-      // var uploader = new qq.FileUploader({
-      //   // pass the dom node (ex. $(selector)[0] for jQuery users)
-      //   element: document.getElementById('file-uploader'),
-      //   // path to server-side upload script
-      //   action: '',
-      //   dragText:"Drag Your File Here",
-      //   multiple:false,
-      //   uploadButtonText: 'Upload File !',
-      //   allowedExtensions: ['csv', 'jpg', 'png'],
-      //   params:{
-      //     "name":""
-      //   },
-      //   showMessage: function(message) {
-      //       $('#file-uploader').append('<div class="alert alert-error">' + message + '</div>');
-      //   },
-      //   onSubmit: function(id, fileName){
-      //   },
-      //   onProgress: function(id, fileName, loaded, total){
 
-      //   },
-      //   onComplete: function(id, fileName, responseJSON){
-          
-      //   },
-      //   onCancel: function(id, fileName){},
-      //   onError: function(id, fileName, xhr){
+    this.uploadFile = function(){
+        var uploader = new qq.FileUploader({
+        // pass the dom node (ex. $(selector)[0] for jQuery users)
+          element: document.getElementById('file-uploader'),
+          // path to server-side upload script
+          action: '',
+          dragText:"Drag Your File Here",
+          multiple:false,
+          uploadButtonText: 'Browse File !',
+          allowedExtensions: ['csv', 'xlxs'],
+          params:{
+            "name":bucketName
+          },
+          showMessage: function(message) {
+              $('#file-uploader').append('<div class="alert alert-error">' + "Invalid file type. Only csv, xlsx files are allowed." + '</div>');
+          },
+          onSubmit: function(id, fileName){
+          },
+          onProgress: function(id, fileName, loaded, total){
 
-      //   }
-      // }); 
-      // console.log(uploader);
-    
+          },
+          onComplete: function(id, fileName, responseJSON){
+            
+          },
+          onCancel: function(id, fileName){},
+          onError: function(id, fileName, xhr){
 
+          }
+      });
+    }
+
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){ 
+     
+       $uibModalInstance.dismiss('cancel');
+      
+    })
 }])
 
 .controller('ImportContactsListController', ['$rootScope','$window', '$state', '$scope', '$http', '$log', '$timeout', 'uiGridConstants','$uibModal', function ($rootScope, $window, $state, $scope, $http, $log, $timeout, uiGridConstants, $uibModal) {
 
-  // $http.get('public/js/controllers/app.json')
-  //   .success(function(data) {
-  //     $scope.gridOptions.data = data;
-  //     $scope.gridOptions.minRowsToShow = data.length;
-  //       $timeout(function() {
-  //       if($scope.gridApi.selection.selectRow){
-  //         $scope.gridApi.selection.selectRow($scope.gridOptions.data[0]);
-  //       }
-  //     });
-  //   });
+  var _this = this;
 
-  // $scope.gridOptions = {
-  //   enableRowSelection: true,
-  //   enableHorizontalScrollbar: 0,
-  //   enableFullRowSelection:true,
-  //   enableSelectAll: true,
-  //   selectionRowHeaderWidth: 35,
-  //   rowSelection: true,
-  //   rowHeight: 35
-  // };
-  
-  // $scope.gridOptions.columnDefs = [
-  //   { name: 'name' ,displayName: "NAME"},
-  //   { name: 'phoneno', displayName: "PHONE NUMBER"},
-  //   { name: 'email', displayName: 'EMAIL'}
-  // ];
- 
-  // $scope.gridOptions.multiSelect = true;
-  // $scope.selectedIndexValue = [];
-  // $scope.gridOptions.onRegisterApi = function(gridApi){
+  var tabNames  = [
+    { id : 1, label : "All" },
+    { id : 2, label : "Employees" },
+    { id : 3, label : "Clients" },
+    { id : 4, label : "Candidates" },
+    { id : 5, label : "iOS_Developers" },
+    { id : 5, label : "Architects" }
+  ];
 
-  //   $scope.gridApi = gridApi;
-  //   gridApi.selection.on.rowSelectionChanged($scope,function(row){
-  //     if($scope.selectedIndexValue.indexOf(row.entity.id)==-1){
-  //        $scope.selectedIndexValue.push(row.entity.id);
-  //      }
-  //     else{
-  //       var index = $scope.selectedIndexValue.indexOf(row.entity.id);
-  //       $scope.selectedIndexValue.splice(index,1);
-  //     }
-  //     console.log( $scope.selectedIndexValue);
-  //     var msg = 'row selected ' + row.isSelected;
-  //     $log.log(msg);
-  //   });
+  this.tabNames = tabNames;
 
-  //   gridApi.selection.on.rowSelectionChangedBatch($scope,function(rows){
-      
-  //     var msg = 'rows changed ' + rows.length;
-  //     $log.log(msg);
-  //   });
-  // };
   var colNamesAll = {
     columnDefs: [
-      { name:'name'},
-      { name:'phoneno',displayName:'Cell Number'},
-      { name:'email'}
+      { name:'id'},
+      { name:'name',displayName:'Name'},
+      { name:'age'}
     ]
   };
 
@@ -151,16 +121,57 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
     ]
   }
 
-  var gridUrlAll = "public/js/controllers/app.json";
+  var All = [
+    {id : 1, type : "emp", name : "John-emp" , age : 23 },
+    {id : 2, type : "emp", name : "John david-emp" , age : 24 },
+    {id : 3, type : "emp", name : "John mickle-emp" , age : 28 },
+    {id : 4, type : "client", name : "willam-client" , age : 32 },
+    {id : 5, type : "client", name : "willam tinker-client" , age : 35 },
+    {id : 6, type : "client", name : "willam crank-client" , age : 33 },
+    {id : 7, type : "candidate", name : "miller-candidate" , age : 44 },
+    {id : 8, type : "candidate", name : "miller killer-candidate" , age : 45 },
+    {id : 9, type : "candidate", name : "miller haddin-candidate" , age : 41 },
+    {id : 10, type : "ios", name : "smith roy-ios" , age : 53 },
+    {id : 11, type : "ios", name : "smith man-ios" , age : 54 },
+    {id : 12, type : "ios", name : "smith hood-ios" , age : 58 },
+  ];
 
-  $scope.importContactGrid = function(url,  colNames, type){
+  var Clients = [
+    {id : 4, type : "client", name : "willam-client" , age : 32 },
+    {id : 5, type : "client", name : "willam tinker-client" , age : 35 },
+    {id : 6, type : "client", name : "willam crank-client" , age : 33 }
+  ];
+
+  var Employees = [
+    {id : 1, type : "emp", name : "John-emp" , age : 23 },
+    {id : 2, type : "emp", name : "John david-emp" , age : 24 },
+    {id : 3, type : "emp", name : "John mickle-emp" , age : 28 }
+  ];
+
+
+  var Candidates = [
+    {id : 7, type : "candidate", name : "miller-candidate" , age : 44 },
+    {id : 8, type : "candidate", name : "miller killer-candidate" , age : 45 },
+    {id : 9, type : "candidate", name : "miller haddin-candidate" , age : 41 }
+  ];
+
+  var iOS_Developers = [
+    {id : 10, type : "ios", name : "smith roy-ios" , age : 53 },
+    {id : 11, type : "ios", name : "smith man-ios" , age : 54 },
+    {id : 12, type : "ios", name : "smith hood-ios" , age : 58 }
+  ];
+
+  var gridUrlAll = 'https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/10000_complex.json';
+
+  this.importContactGrid = function(url,  colNames, id, type){
+
     $scope.allContacts = colNames;
     $scope.gridOptions = {
       infiniteScrollRowsFromEnd: 60,
       enableFullRowSelection:true,
       infiniteScrollUp: true,
       infiniteScrollDown: true,
-      enableHorizontalScrollbar: 0,
+      enableHorizontalScrollbar: 1,
       rowHeight:35,
       data: 'data',
       onRegisterApi: function(gridApi){
@@ -169,26 +180,28 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
         $scope.gridApi = gridApi;
       }
     };
+
     angular.extend($scope.gridOptions,  $scope.allContacts)
 
     $scope.data = [];
-
+ 
     $scope.firstPage = 0;
     $scope.lastPage = 0;
-
+ 
     $scope.getFirstData = function() {
-      return $http.get(url)
+      return $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/10000_complex.json')
       .then(function(response) {
-        var newData = $scope.getPage(response.data[0][type], $scope.lastPage);
+        var newData = $scope.getPage(eval(type), $scope.lastPage);
+        console.log(type)
         $scope.data = $scope.data.concat(newData);
       });
     };
-
+   
     $scope.getDataDown = function() {
-      return $http.get(url)
+      return $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/10000_complex.json')
       .then(function(response) {
         $scope.lastPage++;
-        var newData = $scope.getPage(response.data[0][type], $scope.lastPage);
+        var newData = $scope.getPage(eval(type), $scope.lastPage);
         $scope.gridApi.infiniteScroll.saveScrollPercentage();
         $scope.data = $scope.data.concat(newData);
         return $scope.gridApi.infiniteScroll.dataLoaded($scope.firstPage > 0, $scope.lastPage < 4).then(function() {$scope.checkDataLength('up');});
@@ -197,12 +210,12 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
         return $scope.gridApi.infiniteScroll.dataLoaded();
       });
     };
-
+   
     $scope.getDataUp = function() {
-      return $http.get(url)
+      return $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/10000_complex.json')
       .then(function(response) {
         $scope.firstPage--;
-        var newData = $scope.getPage(response.data[0][type], $scope.firstPage);
+        var newData = $scope.getPage(eval(type), $scope.firstPage);
         $scope.gridApi.infiniteScroll.saveScrollPercentage();
         $scope.data = newData.concat($scope.data);
         return $scope.gridApi.infiniteScroll.dataLoaded($scope.firstPage > 0, $scope.lastPage < 4).then(function() {$scope.checkDataLength('down');});
@@ -211,22 +224,21 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
         return $scope.gridApi.infiniteScroll.dataLoaded();
       });
     };
-
+   
     $scope.getPage = function(data, page) {
-      console.log(page)
       var res = [];
       for (var i = (page * 100); i < (page + 1) * 100 && i < data.length; ++i) {
         res.push(data[i]);
       }
       return res;
     };
-
+   
     $scope.checkDataLength = function( discardDirection) {
       // work out whether we need to discard a page, if so discard from the direction passed in
       if( $scope.lastPage - $scope.firstPage > 3 ){
         // we want to remove a page
         $scope.gridApi.infiniteScroll.saveScrollPercentage();
-
+   
         if( discardDirection === 'up' ){
           $scope.data = $scope.data.slice(100);
           $scope.firstPage++;
@@ -244,23 +256,7 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
         }
       }
     };
-
-    $scope.reset = function() {
-      $scope.firstPage = 2;
-      $scope.lastPage = 2;
-
-      // turn off the infinite scroll handling up and down - hopefully this won't be needed after @swalters scrolling changes
-      $scope.gridApi.infiniteScroll.setScrollDirections( false, false );
-      $scope.data = [];
-
-      $scope.getFirstData().then(function(){
-        $timeout(function() {
-          // timeout needed to allow digest cycle to complete,and grid to finish ingesting the data
-          $scope.gridApi.infiniteScroll.resetScroll( $scope.firstPage > 0, $scope.lastPage < 4 );
-        });
-      });
-    };
-
+ 
     $scope.getFirstData().then(function(){
       $timeout(function() {
         // timeout needed to allow digest cycle to complete,and grid to finish ingesting the data
@@ -269,36 +265,24 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
         $scope.gridApi.infiniteScroll.resetScroll( $scope.firstPage > 0, $scope.lastPage < 4 );
       });
     });
+
   }
-  $scope.importContactGrid(gridUrlAll, colNamesAll,"all");
-  this.allDetails = function(){
-    $scope.importContactGrid(gridUrlAll, colNamesAll,"all");  
-  }
-  this.allemp = function(){
-    $scope.importContactGrid(gridUrlAll, colNamesCommon,"employee");  
-  }
-  this.clients = function(){
-    $scope.importContactGrid(gridUrlAll, colNamesAll,"clients");  
+  //default calling all contacts
+  _this.importContactGrid(gridUrlAll, colNamesAll, 1, "All");
+
+  this.getInfo = function(id ,type){
+      _this.importContactGrid(gridUrlAll, colNamesAll, id, type);
   }
 
-  this.candidates = function(){
-    $scope.importContactGrid(gridUrlAll, colNamesAll,"candidates");  
-  }
-  this.isoDev = function(){
-    $scope.importContactGrid(gridUrlAll, colNamesCommon,"isoDev");  
-  }
-  this.architects = function(){
-    $scope.importContactGrid(gridUrlAll, colNamesCommon,"architects");  
-  }
   this.importSelect = function(){
     $uibModal.open({
       animation: false,
-      templateUrl: 'templates/dialogs/custom-msg.phtml',
+      templateUrl: 'public/templates/dialogs/custom-msg.html',
       controller: 'modalCtrl',
       controllerAs:"ctrl"
     });
-    //$state.go('importContactsList').then(() => $modalInstance.close());
   }
+
   this.backtoUploadCon = function() {
     $window.scrollTo(0,0);
     $state.go('importContacts');
@@ -306,6 +290,7 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
    
 }])
 .controller('InviteContacts', ['$window', '$state', '$scope', '$http', '$log', '$timeout', 'uiGridConstants', '$uibModal', function ($window, $state, $scope, $http, $log, $timeout, uiGridConstants, $uibModal) {
+  
   $scope.gridOptions = {
     enableRowSelection: true,
     enableHorizontalScrollbar: 0,
@@ -351,26 +336,10 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
     });
   }; 
 
-  // this.inviteCustom = function(){
-  //   $uibModal.open({
-  //     animation: true,
-  //     templateUrl: 'public/templates/dialogs/custom-msg.html',
-  //     openedClass: "import_verify",
-  //     controller: 'modalCtrl',
-  //     controllerAs:"ctrl"
-  //   });
-  // } 
-
-  // this.backToImportContact = function(){
-  //   $window.scrollTo(0,0);
-  //   $state.go('importContactsList');
-  // }
-  // this.backToHome = function() {
-  //   $window.scrollTo(0,0);
-  //     $state.go('home');
-  // }
 }])
+
 .controller('modalCtrl',['$uibModalInstance', function ($uibModalInstance) {
+  
   this.successMsg = false;
   this.invite = function () {
       this.successMsg = true;
@@ -378,6 +347,7 @@ angular.module('app.import.contacts', ['ui.grid' ,'ui.grid.selection','ui.grid.i
   window.onhashchange = function() {
     $uibModalInstance.dismiss('cancel');
   }
+
 }]);
 
 }());
