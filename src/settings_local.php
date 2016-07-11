d932e32059c1f5086e514b6638de12500e70b890
himabindu <himabindu@2a077918-f434-4ae4-a93c-d96c61136af1>
2016-07-11 13:07:46 +0000

    changed environment
    git-svn-id: http://svn.enterpi.com/repos/Mintmesh_enterprise/branches/dev_with_slim@8508 2a077918-f434-4ae4-a93c-d96c61136af1

--- src/settings_local.php
+++ src/settings_local.php
@@ -23,13 +23,20 @@ return [
             'NAME' => 'Mintmesh Enterprise',
 
             // Application Version
-            'VERSION' => '1',
+            'VERSION' => 'v1',
+
+            //Api Prefix
+            'PREFIX' => '/enterprise',
 
             // Angular Application domain
-            'APP_DOMAIN' => 'http://192.168.33.10/dev_with_slim/',
+            'APP_DOMAIN' => 'http://192.168.33.10/mintmesh_enterprise/',
 
             // Application API domain
-            'API_DOMAIN' => 'http://192.168.33.10/mintmesh/'
+            'API_DOMAIN' => 'http://192.168.33.10/mintmesh/',
+
+            //Client keys
+            'CLIENT_ID'         => 'G7iLdQoeZy0Ef06C',
+            'CLIENT_SECRET'     => 'Dh0pMLSV6Y82EfDpGKlWN1AyzvWvbvz4'
         ],
         
     ],
