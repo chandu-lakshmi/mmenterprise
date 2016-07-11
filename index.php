419d9124fadc0659e18ba906a0beebf059d5bf51
himabindu <himabindu@2a077918-f434-4ae4-a93c-d96c61136af1>
2016-07-11 13:07:08 +0000

    changed environment
    git-svn-id: http://svn.enterpi.com/repos/Mintmesh_enterprise/branches/dev_with_slim@8507 2a077918-f434-4ae4-a93c-d96c61136af1

--- index.php
+++ index.php
@@ -3,12 +3,17 @@ require __DIR__ . '/vendor/autoload.php';
 
 session_start();
 
+DEFINE("DIR_PATH",__DIR__);
+
 // for setting up global configs
 require __DIR__ . '/src/config.php';
 
 // Instantiate the app
 $settings = require __DIR__ . "/src/settings".$environment.".php";
 $app = new \Slim\App($settings);
+
+//Functions
+require __DIR__. '/src/functions.php';
 
 // Set up dependencies
 require __DIR__ . '/src/dependencies.php';
@@ -18,6 +23,7 @@ require __DIR__ . '/src/middleware.php';
 
 // Register routes
 require __DIR__ . '/src/routes.php';
+
 
 // Run app
 $app->run();
