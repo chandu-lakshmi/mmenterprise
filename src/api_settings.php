95c30e92d795efb758b4096f25e678e39228d3c1
himabindu <himabindu@2a077918-f434-4ae4-a93c-d96c61136af1>
2016-07-14 13:11:22 +0000

    Settings for app
    git-svn-id: http://svn.enterpi.com/repos/Mintmesh_enterprise/branches/dev_with_slim@8547 2a077918-f434-4ae4-a93c-d96c61136af1

---
+++ src/api_settings.php
@@ -0,0 +1,54 @@
+<?php
+
+// API settings
+return [
+		   'login' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/enterprise/login'
+            ],
+            'create_user' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/enterprise/create_user'
+            ],
+            'get_industries' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/get_industries'
+            ],
+            'get_employment_types' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/get_employment_types'
+            ],
+            'get_experiences' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/get_experiences'
+            ],
+            'get_job_functions' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/get_job_functions'
+            ],
+            'update_company' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/enterprise/update_company'
+            ],
+            'test' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/enterprise/test'
+            ],
+            'verify_email' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/enterprise/verify_email'
+            ],
+            'buckets_list' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/enterprise/buckets_list'
+            ],
+            'contact_list' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/enterprise/contact_list'
+            ],
+            'contacts_upload' => [
+                'VERSION'  => 'v1',
+                'ENDPOINT' => '/enterprise/contacts_upload'
+            ],
+    ];
+        
