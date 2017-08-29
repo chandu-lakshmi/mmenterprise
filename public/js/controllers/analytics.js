064da91517fbb4a42fee407f830561f4792cd937
jayakrishna-enterpi <t.jayakrishna@enterpi.com>
2017-08-29 14:42:47 +0530

    changes in link in analytics

--- public/js/controllers/analytics.js
+++ public/js/controllers/analytics.js
@@ -30,25 +30,17 @@
 		var vm = this;
 
 
-		this.recentSearchResults = [
+		this.dropDownList = [
 			{ title : 'Diversity By Gender', desc : 'Gender diversity is equitable or fair representation between genders. Gender diversity most commonly refers to an equitable ratio of men and women, but may also include non-binary gender categories. Its vital to have a good ratio of gender diversity in the organization which brings in increased financial performance better reputation, customer base …', link: 'https://public.tableau.com/profile/saurabho#!/vizhome/EmployeeDiversityDashboard-Gender/GenderDiversityDashboard?publish=yes'},
 			{ title : 'Diversity By Ethnicity', desc : 'Ethnic diversity is fair representation of people in an organization based on age, culture, nation etc. Ethnic diversity brings in more, employee satisfaction, retention, different perspectives, improve the ability to influence.', link: 'https://public.tableau.com/profile/saurabho#!/vizhome/EmployeeDiversityDashboard-Ethnicity/EthnicDiversityDashboard?publish=yes'},
 			{ title : 'Diversity By Religion', desc : 'Religious diversity is an important metric to be considered in an organization as this represents the organization as an organization with equal opportunity for all. Religious diversity enhances the brand as an equal opportunity employer…', link: 'https://public.tableau.com/profile/saurabho#!/vizhome/EmployeeDiversityDashboard-Religion/ReligionDiversityDashboard?publish=yes'},
 			{ title : 'Diversity By Critical Thinking', desc : 'Critical Thinking is the ability of a person to reason through any issue/situation. Having an understanding on the mix of people with critical thinking is critical for the overall success of any organization…', link: 'https://public.tableau.com/profile/saurabho#!/vizhome/EmployeeDiversityDashboard-CriticalThinking/CriticalThinkingDiversityDashboard?publish=yes'}
 		];
 
-		this.dropDownList = [
-			{name:'Diversity By Gender'}, 
-			{name:'Diversity By Ethnicity'}, 
-			{name:'Diversity By Religion'},
-			{name:'Diversity By Critical Thinking'}
-		];
-
-
 		this.localSearch = function(str) {
 			var matches = [];
 		  	vm.dropDownList.forEach(function(list) {
-			    if (list.name.toLowerCase().indexOf(str.toString().toLowerCase()) >= 0) {
+			    if (list.title.toLowerCase().indexOf(str.toString().toLowerCase()) >= 0) {
 			      	matches.push(list);
 			    }
 		  	});
@@ -58,12 +50,13 @@
 		this.search = function() {
 			var txt = $('#search_value').val();
 			if(txt.length){
-				this.selectedResult({ title: txt });
+				$state.go('app.analyticsSearch', { searchVal : txt });
 			}
 		}
 
-		this.selectedResult = function(obj) {
-			$state.go('app.analyticsSearch', { searchVal : obj.title });
+		this.selectedResult = function(selected) {
+			if(selected)
+				window.open(selected.description.link,'_blank');
 		}
 
 
@@ -88,27 +81,20 @@
 
 
 		this.dropDownList = [
-			{name:'Diversity By Gender'}, 
-			{name:'Diversity By Ethnicity'}, 
-			{name:'Diversity By Religion'},
-			{name:'Diversity By Critical Thinking'}
-		];
-		this.searchResults = [
 			{ title : 'Diversity By Gender', desc : 'Gender diversity is equitable or fair representation between genders. Gender diversity most commonly refers to an equitable ratio of men and women, but may also include non-binary gender categories. Its vital to have a good ratio of gender diversity in the organization which brings in increased financial performance better reputation, customer base …', link: 'https://public.tableau.com/profile/saurabho#!/vizhome/EmployeeDiversityDashboard-Gender/GenderDiversityDashboard?publish=yes'},
 			{ title : 'Diversity By Ethnicity', desc : 'Ethnic diversity is fair representation of people in an organization based on age, culture, nation etc. Ethnic diversity brings in more, employee satisfaction, retention, different perspectives, improve the ability to influence.', link: 'https://public.tableau.com/profile/saurabho#!/vizhome/EmployeeDiversityDashboard-Ethnicity/EthnicDiversityDashboard?publish=yes'},
 			{ title : 'Diversity By Religion', desc : 'Religious diversity is an important metric to be considered in an organization as this represents the organization as an organization with equal opportunity for all. Religious diversity enhances the brand as an equal opportunity employer…', link: 'https://public.tableau.com/profile/saurabho#!/vizhome/EmployeeDiversityDashboard-Religion/ReligionDiversityDashboard?publish=yes'},
 			{ title : 'Diversity By Critical Thinking', desc : 'Critical Thinking is the ability of a person to reason through any issue/situation. Having an understanding on the mix of people with critical thinking is critical for the overall success of any organization…', link: 'https://public.tableau.com/profile/saurabho#!/vizhome/EmployeeDiversityDashboard-CriticalThinking/CriticalThinkingDiversityDashboard?publish=yes'}
 		];
 		
-		this.selectedResult = function(obj) {
-			
-			if(obj){
-				vm.search = obj.title;
+		this.selectedResult = function(selected) {
+			if(selected){
+				vm.search = selected.title;
+				window.open(selected.description.link,'_blank');
 			} else{
 				vm.search = $('#search_value').val();
 			}
 			hasDropdownSelelected = true;
-
 		}
 
 		this.localSearch = function(str) {
@@ -122,7 +108,7 @@
 
 			var matches = [];
 		  	vm.dropDownList.forEach(function(list) {
-			    if (list.name.toLowerCase().indexOf(str.toString().toLowerCase()) >= 0) {
+			    if (list.title.toLowerCase().indexOf(str.toString().toLowerCase()) >= 0) {
 			      	matches.push(list);
 			    }
 		  	});
