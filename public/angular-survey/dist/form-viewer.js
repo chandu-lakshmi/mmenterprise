8598cda321d07d6f5bcbcd50ba8a83c2e527eed3
ananya <j.karthik@enterpi.com>
2017-09-14 15:19:22 +0530

    questionnaire

--- public/angular-survey/dist/form-viewer.js
+++ public/angular-survey/dist/form-viewer.js
@@ -380,30 +380,10 @@ angular.module('mwFormViewer').directive('mwFormViewer', ["$rootScope", function
     };
 }]);
 
-angular.module('mwFormViewer').filter('hhmmss', function () {
-  return function (time) {
-    var sec_num = parseInt(time, 10); // don't forget the second param
-    var hours   = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+':'+minutes+':'+seconds;
-    return time;
-  }
-});
-
 angular.module('mwFormViewer').directive('timer', function($timeout, $compile) {
   return {
     restrict: 'E',
     transclude: true,
-    /*controllerAs: 'ctrl',
-    bindToController: true,
-    controller: function(){
-        var ctrl = this;
-    },*/
     scope: {
       interval: '=', //don't need to write word again, if property name matches HTML attribute name
       startTimeAttr: '=?startTime', //a question mark makes it optional
